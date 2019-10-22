ac602e1eb1a50595b51eaa54ceb78e4ea756e0d5
Linus <linusp.93@gmail.com>
2019-10-22 13:52:45 +0200

    skickar encodervärde (heltal mellan 0-1023) till ros

--- src/skogstomten/testrig/arduino_code/controller/controller.ino
+++ src/skogstomten/testrig/arduino_code/controller/controller.ino
@@ -1,68 +1,70 @@
 #include <ros.h>
 #include <std_msgs/Int64.h>
 #include <stdio.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_GFX.h>
-
-// OLED pins
-#define OLED_CLK    12
-#define OLED_MOSI   11
-#define OLED_RESET  10
-#define OLED_DC     9
-#define OLED_CS     8
-
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); //configuring the display
+//#include <SPI.h>
+//#include <Wire.h>
+//#include <Adafruit_SSD1306.h>
+//#include <Adafruit_GFX.h>
+//
+//// OLED pins
+//#define OLED_CLK    12
+//#define OLED_MOSI   11
+//#define OLED_RESET  10
+//#define OLED_DC     9
+//#define OLED_CS     8
+//
+//Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS); //configuring the display
 
 
 int pwm_pins[] = {2,3,4,5,46,44};
 int pin_numbers[] = {35,33,31,29,45,43,41,39,49,51,25,27};
 char pin_data[] = {0,0,0,0,0,0,0,0,0,0,0,0};
 
+int readSpeed;
+
 // time vars
 long long last_callback; // timestamp for last callback
 int timeout = 100; // millis to timeout (->idle state)
 int loop_rate = 100;
 
-// display wrapper
-void updateDisplay()
-{
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-
-  // make title
-  display.setCursor(8,4);
-  display.print("MOTOR CTRL by Misza, edited by HK Auto2");
-  display.setCursor(45,12);
-  display.println("v0.5.0");
-
-  // table titles
-  display.setCursor(13,39);
-  display.print("en");
-  display.setCursor(7,51);
-  display.print("dir"); //should become PWM?
-
-  // print table with enable and dir for each motor
-  for(int i=0; i<4; i++)
-  {
-    display.setTextColor(WHITE);
-    display.setCursor(36+i*23,27);
-    display.print("M");
-    display.print(i+1);
-    display.setCursor(41+i*23,39);
-    display.print(pin_data[i]+0);
-    display.setCursor(41+i*23,51);
-    display.print(pin_data[i+4]+0);
-  }
-  // frame
-  display.drawLine(1,1,126,1,2);
-  display.drawLine(126,1,126,62,2);
-  display.drawLine(126,62,1,62,2);
-  display.drawLine(1,62,1,1,2);
-  display.display();
-}
+//// display wrapper
+//void updateDisplay()
+//{
+//  display.clearDisplay();
+//  display.setTextColor(WHITE);
+//  display.setTextSize(1);
+//
+//  // make title
+//  display.setCursor(8,4);
+//  display.print("MOTOR CTRL by Misza, edited by HK Auto2");
+//  display.setCursor(45,12);
+//  display.println("v0.5.0");
+//
+//  // table titles
+//  display.setCursor(13,39);
+//  display.print("en");
+//  display.setCursor(7,51);
+//  display.print("dir"); //should become PWM?
+//
+//  // print table with enable and dir for each motor
+//  for(int i=0; i<4; i++)
+//  {
+//    display.setTextColor(WHITE);
+//    display.setCursor(36+i*23,27);
+//    display.print("M");
+//    display.print(i+1);
+//    display.setCursor(41+i*23,39);
+//    display.print(pin_data[i]+0);
+//    display.setCursor(41+i*23,51);
+//    display.print(pin_data[i+4]+0);
+//  }
+//  // frame
+//  display.drawLine(1,1,126,1,2);
+//  display.drawLine(126,1,126,62,2);
+//  display.drawLine(126,62,1,62,2);
+//  display.drawLine(1,62,1,1,2);
+//  display.display();
+//}
 
 // simple macro to update motor pins
 void setMotors()
@@ -131,7 +133,8 @@ void pwm_callback( const std_msgs::Int64& pwm_msg ){
 ros::NodeHandle nh;               // Nodehandle is an object representing the ROS node, start the ROS node
 ros::Subscriber<std_msgs::Int64> pin_sub("pins", motor_callback); // Subrscription plan??
 ros::Subscriber<std_msgs::Int64> pwm_sub("change_pwm", pwm_callback);
- 
+std_msgs::Int64 speed_out;
+ros::Publisher speed_publish("speed", &speed_out); 
 
 
 void setup()
@@ -140,13 +143,14 @@ void setup()
   nh.initNode();    
   nh.subscribe(pin_sub);
   nh.subscribe(pwm_sub);
+  nh.advertise(speed_publish);
 
   // display init
   pinMode(13,OUTPUT);
   digitalWrite(13,1); // terrible hack feeding display VCC from pin 13
-  display.begin(SSD1306_SWITCHCAPVCC);
-  display.clearDisplay();
-  display.display();
+//  display.begin(SSD1306_SWITCHCAPVCC);
+//  display.clearDisplay();
+//  display.display();
 
   // motor pin init macro
   for(int i=0; i<12; i++)
@@ -173,8 +177,12 @@ void loop()
   }
 
   // update ROS, update motors, update display, sleep
+  
+  readSpeed = analogRead(A11);
+  speed_out.data = readSpeed;
+  speed_publish.publish(&speed_out);
   nh.spinOnce();
   setMotors();
-  updateDisplay();
+  //updateDisplay();
   delay(loop_rate);
 }
